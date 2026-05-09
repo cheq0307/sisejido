0aa3e1ba523520ff505de7f20cec5d1a748010cc
Ezequiel <cheq.0307@gmail.com>
2026-05-09 12:46:20 -0600

    fix: slidebar

--- resources/views/EditViews/editarApoyo.blade.php
+++ resources/views/EditViews/editarApoyo.blade.php
@@ -1,5 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuevo Apoyo Social</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/estiloPrincipal.css') }}">
+</head>
+<body>
+
 @include('IncludeViews.cabeza')
+
+<div class="container-fluid">
+<div class="row">
 @include('IncludeViews.menu')
+
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
 
 <div class="mt-4">
     <div class="card card-ejidal shadow">
@@ -127,3 +144,7 @@
 </div>
 
 @include('IncludeViews.pie')
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
